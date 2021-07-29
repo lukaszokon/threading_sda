7ad8e0811a0e1b22fdf9bf356e5fb8e466df88ab
Łukasz Okoń <l.okon@haso.pl>
2021-07-29 19:00:48 +0200

    add threading 2nd example

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
 import threading
+import time
+
+
+class ThreadWithReturnValue(threading.Thread):
+    def __init__(self, target, args=(), kwargs=None):
+        if kwargs is None:
+            kwargs = {}
+        self.target = target
+        self.args = args
+        self.kwargs = kwargs
+        self.result = None
+        super().__init__()
+
+    def run(self):
+        self.result = self.target(*self.args, **self.kwargs)
+
+    def join(self, timeout=None):
+        super().join(timeout)
+        return self.result
+
+
+def print_cube(num):
+    time.sleep(5)
+    print(f'Cube: {num ** 3}')
+
+
+def print_square(num):
+    time.sleep(5)
+    return num ** 2
 
 
 def iterate_print(iter):
@@ -7,13 +36,13 @@ def iterate_print(iter):
 
 
 if __name__ == '__main__':
-    t1 = threading.Thread(target=iterate_print, args=(range(5),))
-    t2 = threading.Thread(target=iterate_print, args=('Python',))
+    t1 = ThreadWithReturnValue(target=print_square, args=(10,))
+    t2 = threading.Thread(target=print_cube, args=(10,))
 
     t1.start()
     t2.start()
 
-    t1.join()
+    print(t1.join())
     t2.join()
 
     print('Koniec programu')
