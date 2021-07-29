507323f4e474c0e80de8e169618be30fea3c9459
Łukasz Okoń <l.okon@haso.pl>
2021-07-29 18:29:29 +0200

    add threading 1st example

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
+import threading
+
+
+def iterate_print(iter):
+    for item in iter:
+        print(item)
+
+
 if __name__ == '__main__':
-    print('PyCharm')
+    t1 = threading.Thread(target=iterate_print, args=(range(5),))
+    t2 = threading.Thread(target=iterate_print, args=('Python',))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    print('Koniec programu')
